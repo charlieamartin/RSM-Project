4805e6f1bd958e83145bf08238e17d5435dfd634
charlieamartin <charlieam@outlook.com>
2018-07-02 09:46:20 -0500

    Standardized Spacing and Checked Spellings
    The spacing in the python file was standardized to allow ease of reading.  spelling was also checked and some general comments were made.

--- OhioRiverBasin-DrainageArea_Validation.py
+++ OhioRiverBasin-DrainageArea_Validation.py
@@ -6,12 +6,12 @@ import urllib.request, urllib.error, urllib.parse
 import re
 import matplotlib.pyplot as plt
 import numpy as np
-from math import log10,pow
+from math import log10, pow
 
 #
 # compute linear regression of log10 values
 #
-def compPowerLaw(xin,yin,npts):
+def compPowerLaw(xin, yin, npts):
     '''
     Compute power law coefficients by computing the linear regression of log10 of input values
     Inputs:
@@ -25,18 +25,18 @@ def compPowerLaw(xin,yin,npts):
     '''
     xl = [log10(x) for x in xin]
     yl = [log10(y) for y in yin]
-    a,b = np.polyfit(xl, yl, 1)
+    a, b = np.polyfit(xl, yl, 1)
 
     # compute points on line for plotting - evenly spaced in log space
     xmin = min(xin)
     xmax = max(xin)
-    dx = (log10(xmax)- log10(xmin))/(npts-1)
-    plawx = [10.**(log10(xmin) + i*dx) for i in range(npts)]
+    dx = (log10(xmax) - log10(xmin)) / (npts - 1)
+    plawx = [10.**(log10(xmin) + i * dx) for i in range(npts)]
     # Non Log space calculation
     #dx = (xmax- xmin)/(npts-1)
     #plawx = [xmin + i*dx for i in range(npts)]
-    plawy = [(10.**b)*(x**a) for x in plawx]
-    pts = [plawx,plawy]
+    plawy = [(10.**b) * (x**a) for x in plawx]
+    pts = [plawx, plawy]
 
     return (a, b, pts)
 
@@ -59,17 +59,17 @@ def biasCorrect(sedx, sedy, a, b):
     '''
     np_sedx = np.array(sedx)
     np_sedy = np.array(sedy)
-    tmp = (10**b)*np_sedx**a
+    tmp = (10**b) * np_sedx**a
 
     #compute epsilon from Eq (4) of Kao et al
     epsilon = np_sedy - tmp
 
     #Calculate beta bias-correction factor Eq. (5) of Kao et al
-    beta = sum(epsilon)/sum(tmp)
-    print("beta =",beta)
+    beta = sum(epsilon) / sum(tmp)
+    print("beta =", beta)
 
     # compute modified concentration values from the paper equation using the x values of the data points
-    qhat = (1+beta)*(10.**b)*np_sedx**a
+    qhat = (1+beta) * (10.**b) * np_sedx**a
 
     #file_csv.write("{0},{1},{2},{3},{4},{5}\n".format(siteno, a, b, sedmin[-1],sedmax[-1],beta))
 
@@ -122,11 +122,11 @@ with open(filename) as csvfile:
 #
 
 file_csv1 = open('Global_Error_Validation.csv', 'w')
-file_csv1.write("{0},{1}\n".format("Absolute Error","Relative Error"))
+file_csv1.write("{0},{1}\n".format("Absolute Error", "Relative Error"))
 file_csv2 = open('lessthan200_Error_Validation.csv', 'w')
-file_csv2.write("{0},{1}\n".format("Absolute Error","Relative Error"))
+file_csv2.write("{0},{1}\n".format("Absolute Error", "Relative Error"))
 file_csv3 = open('greaterthan200_Error_Validation.csv', 'w')
-file_csv3.write("{0},{1}\n".format("Absolute Error","Relative Error"))
+file_csv3.write("{0},{1}\n".format("Absolute Error", "Relative Error"))
 
 #
 #  get data from USGS corresponding to sites in the csv file
@@ -135,10 +135,10 @@ file_csv3.write("{0},{1}\n".format("Absolute Error","Relative Error"))
     #siteno = sitenum[i]
     #drainsize = drainarea[i]
 
-for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
+for siteno, drainsize in zip(sitenum[1:], drainarea[1:]):
     if not isfloat(drainsize):
         print('   ')
-        print(siteno,'with drainange are = ',drainsize)
+        print(siteno, 'with drainange area = ', drainsize)
         print('   ')
         continue
 
@@ -161,13 +161,13 @@ for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
     '''
 
     #Sediment measurements
-    codes='parm_cds=00061%2C70331%2C80154&qw'   #retrieving 00061, 70331, and 80154
+    codes = 'parm_cds=00061%2C70331%2C80154&qw'   #retrieving 00061, 70331, and 80154
 
-    url='https://nwis.waterdata.usgs.gov/nwis/qwdata/?site_no='
-    url=url + siteno
-    url=url + '&agency_cd=USGS&inventory_output=0&rdb_inventory_output=file&TZoutput=0&pm_cd_compare=Greater%20than&radio_parm_cds=parm_cd_list&radio_multiple_'
-    url=url + codes
-    url=url + '_attributes=0&format=rdb&qw_sample_wide=wide&rdb_qw_attributes=0&date_format=YYYY-MM-DD&rdb_compression=value&submitted_form=brief_list'
+    url = 'https://nwis.waterdata.usgs.gov/nwis/qwdata/?site_no='
+    url = url + siteno
+    url = url + '&agency_cd=USGS&inventory_output=0&rdb_inventory_output=file&TZoutput=0&pm_cd_compare=Greater%20than&radio_parm_cds=parm_cd_list&radio_multiple_'
+    url = url + codes
+    url = url + '_attributes=0&format=rdb&qw_sample_wide=wide&rdb_qw_attributes=0&date_format=YYYY-MM-DD&rdb_compression=value&submitted_form=brief_list'
     page = urllib.request.urlopen(url)
 
     # Decode required to ensure the page can be read
@@ -185,18 +185,18 @@ for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
     llist = page_content.split('\n')
     for line in llist:
         # split the line into words separated by tabs
-        words = line.replace('\r','').split('\t')
+        words = line.replace('\r', '').split('\t')
 
         # discharge
-        if re.match("#\s+P00061",line):
+        if re.match("#\s+P00061", line):
             p6line = line
             p6flag = True
         # suspended sediment
-        elif re.match("#\s+P70331",line):
+        elif re.match("#\s+P70331", line):
             p7line = line
             p7flag = True
         # suspended sediment concentration
-        elif re.match("#\s+P80154",line):
+        elif re.match("#\s+P80154", line):
             p8line = line
             p8flag = True
 
@@ -219,12 +219,12 @@ for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
     #
     for line in llist:
         # split the line into words separated by tabs
-        words = line.replace('\r','').split('\t')
+        words = line.replace('\r', '').split('\t')
 
         #
         # find the header record
         #
-        if p6flag and p7flag and p8flag and re.match("agency_cd",line):
+        if p6flag and p7flag and p8flag and re.match("agency_cd", line):
 
             # find the indices of the discharge and sediment data
             i6 = words.index('p00061')
@@ -232,22 +232,22 @@ for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
             i8 = words.index('p80154')
 
         # the data lines start with USGS
-        if re.match("\AUSGS",line):
-            if len(words[i6])>0 and len(words[i8])>0:
+        if re.match("\AUSGS", line):
+            if len(words[i6]) > 0 and len(words[i8]) > 0:
                 w6 = words[i6].strip('EA<>')
                 w7 = words[i7].strip('EA<>')
                 w8 = words[i8].strip('EA<>')
                 dis = float(w6)
                 con = float(w8)
                 #print(" sed {0:12.3f}{1:>12}{2:12.3f}".format(dis,"  -  ",con))
-                if (dis !=0.0) and (con != 0.0):
+                if (dis != 0.0) and (con != 0.0):
                    sedx.append(dis)
                    sedy.append(con)
     #
     # perform linear regression by polynomial fitting
     #
 
-    a,b, pts = compPowerLaw(sedx, sedy, 5)
+    a, b, pts = compPowerLaw(sedx, sedy, 5)
     plaws.append(pts)
     if drainsize >= 200:
         print('> 200 drainsize = ', drainsize)
@@ -260,7 +260,7 @@ for siteno, drainsize in zip(sitenum[1:],drainarea[1:]):
         sedxl200.append(sedx)
         sedyl200.append(sedy)
 
-    print('      1st power law coeffs a,b = ',a,b )
+    print('      1st power law coeffs a, b = ', a, b)
 
 
 plawxall = []
@@ -269,9 +269,9 @@ for plaw in plaws:
     plawxall.extend(plaw[0])
     plawyall.extend(plaw[1])
 
-a,b, pts = compPowerLaw(plawxall,plawyall,5)
+a, b, pts = compPowerLaw(plawxall, plawyall, 5)
 print('   ')
-print('Universal power law coeffs a,b = ',a,b )
+print('Universal power law coeffs a, b = ', a, b)
 
 #plt.figure()
 #plt.loglog(plawxall, plawyall, 'g*')
@@ -284,8 +284,8 @@ np_plawxall = np.array(plawxall)
 np_plawyall = np.array(plawyall)
 
 #convert from (mg/L)*(ft^3/sec) to (ton/day) multiply by 0.002698
-error_all = 0.002698*(qhat*np_plawxall - np_plawyall*np_plawxall)
-rela_error_all= error_all/(np_plawyall*np_plawxall*0.002698)
+error_all = 0.002698 * (qhat * np_plawxall - np_plawyall * np_plawxall)
+rela_error_all = error_all / (np_plawyall * np_plawxall * 0.002698)
 
 for i in range(len(error_all)):
     file_csv1.write("{0},{1}\n".format(error_all[i],rela_error_all[i]))
@@ -313,9 +313,9 @@ for plawl in plawl200:
     plawlxall.extend(plawl[0])
     plawlyall.extend(plawl[1])
 
-a,b, pts = compPowerLaw(plawlxall,plawlyall,5)
+a, b, pts = compPowerLaw(plawlxall ,plawlyall, 5)
 print('   ')
-print('    < 200 power law coeffs a,b = ',a,b )
+print('    < 200 power law coeffs a, b = ', a, b)
 
 #plt.loglog(plawlxall, plawlyall, 'b*')
 #plt.loglog(pts[0], pts[1], 'b*-')
@@ -330,8 +330,8 @@ np_plawlxall = np.array(plawlxall)
 np_plawlyall = np.array(plawlyall)
 
 #convert from (mg/L)*(ft^3/sec) to (ton/day) multiply by 0.002698
-error_all = 0.002698*(qhat*np_plawlxall - np_plawlyall*np_plawlxall)
-rela_error_all= error_all/(np_plawlyall*np_plawlxall*0.002698)
+error_all = 0.002698 * (qhat * np_plawlxall - np_plawlyall * np_plawlxall)
+rela_error_all = error_all / (np_plawlyall * np_plawlxall * 0.002698)
 
 for i in range(len(error_all)):
     file_csv2.write("{0},{1}\n".format(error_all[i],rela_error_all[i]))
@@ -355,9 +355,9 @@ for plawg in plawg200:
     plawgxall.extend(plawg[0])
     plawgyall.extend(plawg[1])
 
-a,b, pts = compPowerLaw(plawgxall,plawgyall,5)
+a, b, pts = compPowerLaw(plawgxall, plawgyall, 5)
 print('   ')
-print('    > 200 power law coeffs a,b = ',a,b )
+print('    > 200 power law coeffs a, b = ', a, b)
 
 #plt.loglog(plawgxall, plawgyall, 'r*')
 #plt.loglog(pts[0], pts[1], 'r*-')
@@ -372,8 +372,8 @@ np_plawgxall = np.array(plawgxall)
 np_plawgyall = np.array(plawgyall)
 
 #convert from (mg/L)*(ft^3/sec) to (ton/day) multiply by 0.002698
-error_all = 0.002698*(qhat*np_plawgxall - np_plawgyall*np_plawgxall)
-rela_error_all= error_all/(np_plawgyall*np_plawgxall*0.002698)
+error_all = 0.002698 * (qhat * np_plawgxall - np_plawgyall * np_plawgxall)
+rela_error_all = error_all / (np_plawgyall * np_plawgxall * 0.002698)
 
 for i in range(len(error_all)):
     file_csv3.write("{0},{1}\n".format(error_all[i],rela_error_all[i]))
